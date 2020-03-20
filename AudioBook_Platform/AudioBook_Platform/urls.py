1feeb838ff933e0867ac9c5c97fc49c75930185d
Tory.Pleiades <xietaorui1999@outlook.com>
2020-03-20 18:07:09 +0800

    url update

--- AudioBook_Platform/AudioBook_Platform/urls.py
+++ AudioBook_Platform/AudioBook_Platform/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('backstage/', include('AudioBook.urls'))
+    path('', include('AudioBook.urls'))
 ]
