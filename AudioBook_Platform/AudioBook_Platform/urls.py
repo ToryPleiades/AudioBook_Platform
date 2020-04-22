42da1d4b1251bc9719b9bde912690ebfb1ab6bb8
Tory.Pleiades <xietaorui1999@outlook.com>
2020-04-22 17:05:22 +0800

    fix bug

--- AudioBook_Platform/AudioBook_Platform/urls.py
+++ AudioBook_Platform/AudioBook_Platform/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('AudioBook.urls'))
+    path('', include('AudioBook.urls')),
+    url(r'static/(?P<path>.*)', serve, {'document_root': settings.MEDIA_ROOT})
 ]
